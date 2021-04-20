129ee89a9b4bbe4f455e7b458edda8b30135408d
Wendy Harris <wendyv510@hotmail.com>
2021-04-20 17:32:53 -0400

    font awesome

--- Desktop/portfolio/src/components/Navbar.js
+++ Desktop/portfolio/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -7,7 +8,7 @@ const Navbar = () => {
  
   <a className="navbar-brand" href="#">Navbar</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <FontAwesomeIcon icon={faBars} />
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
