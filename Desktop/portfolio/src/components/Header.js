be6fb0b2332e7774ccf6e9476985074048864a5d
Wendy Harris <wendyv510@hotmail.com>
2021-04-21 12:16:50 -0400

    header wording

--- Desktop/portfolio/src/components/Header.js
+++ Desktop/portfolio/src/components/Header.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React from "react";
+import Typed from "react-typed";
 
 function Header() {
     return (
         <div className="header-wrapper">
-            <h1> </h1>
+            <div className="main-info">
+              <h1> Wendy Harris Software Engineer </h1>
+              <Typed 
+                 className="typed-text"
+                 strings={["Web Desing", "Web Development"]}
+                 typeSpeed={40}
+                 backSpeed={60}
+              />
+            </div>
         </div>
     )
 }
