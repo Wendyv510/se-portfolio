460f9477ca4da028aee8fd207d68491d7441f468
Wendy Harris <wendyv510@hotmail.com>
2021-06-30 14:43:39 -0400

    about skeleton

---
+++ Desktop/portfolio/src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+    return (
+        <div className="about-wrapper">
+            <div className="about">
+              
+            </div>
+        </div>
+    )
+}
+
+export default About
