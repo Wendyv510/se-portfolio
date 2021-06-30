0ccfc3574d2fd80a9192dad4b8afc0e8a9f6e877
Wendy Harris <wendyv510@hotmail.com>
2021-06-30 16:49:56 -0400

    about pic

--- Desktop/portfolio/src/components/About.js
+++ Desktop/portfolio/src/components/About.js
@@ -5,6 +5,11 @@ function About() {
         <div className="about-wrapper">
             <div className="about">
               <h1> About </h1>
+              <div className="picture"> 
+                <img  src={`${process.env.PUBLIC_URL}/picture.png.jpg`}
+                      alt="profile picture" 
+                /> 
+              </div>
             </div>
         </div>
     )
