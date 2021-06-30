2c96dae9add7a14223fbca61c96b2779a027f272
Wendy Harris <wendyv510@hotmail.com>
2021-06-30 17:07:20 -0400

    about css

--- Desktop/portfolio/src/components/About.js
+++ Desktop/portfolio/src/components/About.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function About() {
     return (
-        <div className="about-wrapper">
-            <div className="about">
+        <div className="about">
+            <div className="intro">
               <h1> About </h1>
               <div className="picture"> 
                 <img  src={`${process.env.PUBLIC_URL}/picture.png.jpg`}
