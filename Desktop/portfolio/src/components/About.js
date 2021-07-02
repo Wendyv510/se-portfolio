f30d8d73d356dedbaf07a4c75f57fcb771be4090
Wendy Harris <wendyv510@hotmail.com>
2021-07-02 10:15:18 -0400

    about

--- Desktop/portfolio/src/components/About.js
+++ Desktop/portfolio/src/components/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className="about">
             <div className="intro">
-              <h1> About </h1>
+              <h1> Hello - my name is Wendy Harris, I am a daycare provider/preschool Teacher turned Techy/Software Engineer.  </h1>
               <div className="picture"> 
                 <img  src={`${process.env.PUBLIC_URL}/picture.png.jpg`}
                       alt="profile picture" 
