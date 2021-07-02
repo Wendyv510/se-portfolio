a4fe7bc1aa116bdd5d5bfb8320d6d959034f105d
Wendy Harris <wendyv510@hotmail.com>
2021-07-02 12:07:23 -0400

    footer links and icons

--- Desktop/portfolio/src/components/Footer.js
+++ Desktop/portfolio/src/components/Footer.js
@@ -3,7 +3,21 @@
 const Footer = () => {
     return (
         <footer> 
-            <h1>Footer </h1>
+            < a href="https://www.linkedin.com/in/wendy-harris-69391379/"
+                target="_blank"> 
+                <img src = "https://1000logos.net/wp-content/uploads/2017/03/Linkedin-Logo.png" 
+                     alt="LinkedIn-icon" />
+                 </a>
+            < a href="https://www.facebook.com/wendy.vosburgh"
+                target="_blank"> 
+                <img src ="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png" 
+                     alt="Facebook-icon" /> 
+                </a> 
+            < a href="https://github.com/Wendyv510" 
+                target="_blank">
+                <img src = "https://cdn.iconscout.com/icon/free/png-512/github-154-675675.png" 
+                     alt="Github=icon" /> 
+                </a>
         </footer>
     )
 }
