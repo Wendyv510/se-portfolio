2fc682f032996aec14fa71f5aa78764a8b2313b4
Wendy Harris <wendyv510@hotmail.com>
2021-07-02 15:15:04 -0400

    router switch

--- Desktop/portfolio/src/App.js
+++ Desktop/portfolio/src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './App.css'; 
-import { BrowserRouter as Router, Route, } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"; 
 import Navbar from "./components/Navbar";
 import Header from "./components/Header" 
@@ -14,6 +14,12 @@ function App() {
   return (
     <Router>
       <Navbar />
+        <Switch>
+          <Route exact path="/Header" component={Header} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Resume" component={Resume} />
+          <Route exact path="/ContactMe" component={ContactMe} />
+        </Switch>
       <Header /> 
       <About />
       <Footer />
