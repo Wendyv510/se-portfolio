084b4c5ca53146db282390c795bb6094f67c31d8
Wendy Harris <wendyv510@hotmail.com>
2021-07-06 14:53:40 -0400

    misc

--- Desktop/portfolio/src/components/ContactMe.js
+++ Desktop/portfolio/src/components/ContactMe.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState }  from "react";
 
 function ContactMe() {
     return (
         <div className="contactme">
-            <div className="about">
+            <div className="contact">
               <h1> Contact Me  </h1>
             </div>
         </div>
