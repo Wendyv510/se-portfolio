0fc3bb0634c87a2f3813467e56c58494b6d6f771
Wendy Harris <wendyv510@hotmail.com>
2021-07-29 09:55:10 -0400

    removing resume

--- Desktop/portfolio/src/App.js
+++ Desktop/portfolio/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Switch, Route, } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"; 
 import Navbar from "./components/Navbar";
 import Header from "./components/Header" 
-import Resume from "./components/Resume"
 import About from "./components/About"
 import ContactMe from "./components/ContactMe"
 import Footer from "./components/Footer"
@@ -17,13 +16,11 @@ function App() {
         <Switch>
           <Route exact path="/Header" component={Header} />
           <Route exact path="/About" component={About} />
-          <Route exact path="/Resume" component={Resume} />
           <Route exact path="/ContactMe" component={ContactMe} />
         </Switch>
       <Header /> 
       <About />
       <Footer />
-      <Resume />
       <ContactMe /> 
      
     </Router>
