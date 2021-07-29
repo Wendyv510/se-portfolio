cc0394ff909089d70364e973c2d4f7b7f2b6eb9b
Wendy Harris <wendyv510@hotmail.com>
2021-07-29 16:29:12 -0400

    blog background pic and cleanup index.js

--- Desktop/portfolio/src/App.js
+++ Desktop/portfolio/src/App.js
@@ -8,6 +8,7 @@ import Header from "./components/Header"
 import About from "./components/About"
 import ContactMe from "./components/ContactMe"
 import Footer from "./components/Footer"
+import Blog from "./components/Blog" 
 import{ init } from 'emailjs-com';
 init("user_A6LvA340P4nCzBwlFdqRG");
 
@@ -18,10 +19,12 @@ function App() {
         <Switch>
           <Route exact path="/Header" component={Header} />
           <Route exact path="/About" component={About} />
+          <Route exact path="/Blog" component={Blog} /> 
           <Route exact path="/ContactMe" component={ContactMe} />
         </Switch>
       <Header /> 
       <About />
+      <Blog />
       <Footer />
       <ContactMe /> 
      
