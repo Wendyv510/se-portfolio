f1cb434df7450ae56450e8f77c2cf00952e6f61f
Wendy Harris <wendyv510@hotmail.com>
2021-09-21 13:58:13 -0400

    configuration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
 import React from 'react';
 import './App.css'; 
-import { BrowserRouter as Router, Switch, Route, } from "../react-router-dom"
-import "../bootstrap/dist/css/bootstrap.min.css"; 
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"; 
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header" 
 import About from "./Components/About"
 import ContactMe from "./Components/ContactMe"
 import Footer from "./Components/Footer"
 import Blog from "./Components/Blog" 
-import{ init } from '../emailjs-com';
+import{ init } from 'emailjs-com';
 init("user_A6LvA340P4nCzBwlFdqRG");
 
 function App() {
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
-//import React from "react"
-//import { Link } from "react-router-dom" 
+import React from "react"
+import { Link } from "react-router-dom" 
 //import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
 //import { faBars } from "/@fortawesome/free-solid-svg-icons"
 
