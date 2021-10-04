97a2a3b6be77b21793e7f9e18c3f68052a1d94ff
Wendy Harris <wendyv510@hotmail.com>
2021-10-04 16:56:52 -0400

    fort awesome

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom" 
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
-//import { faBars } from "/@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
+import { faBars } from "/@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
     return (
