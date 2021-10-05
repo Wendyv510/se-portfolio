19c74ecc1952422617b18fefc7cb5fc1eb270caa
Wendy Harris <wendyv510@hotmail.com>
2021-10-05 11:30:57 -0400

    fontawesome

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,10 +9,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
  <div className="container">
  
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <FontAwesomeIcon icon={faBars} />
-  </button>
-
+  
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item px-3">
