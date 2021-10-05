6124a4395cf66a7c020b713680da0f3d498b1ab3
Wendy Harris <wendyv510@hotmail.com>
2021-10-05 11:39:23 -0400

    fontawesome

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom" 
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
-//import { faBars } from "/@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
+import { faBars } from "/@fortawesome/free-solid-svg-icons"
 
 
 const Navbar = () => {
@@ -9,7 +9,10 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
  <div className="container">
  
-  
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <FontAwesomeIcon icon={faBars} />
+  </button>
+
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item px-3">
